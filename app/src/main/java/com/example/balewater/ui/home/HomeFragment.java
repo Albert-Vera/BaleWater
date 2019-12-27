a538998528c4ef511275ed6e6c8391584048c3f5
albert <avera@elpuig.xeill.net>
2019-12-27 19:44:59 +0100

    Initial commit

---
+++ app/src/main/java/com/example/balewater/ui/home/HomeFragment.java
@@ -0,0 +1,69 @@
+package com.example.balewater.ui.home;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.balewater.MainActivity;
+import com.example.balewater.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+    TextView animTextView, animTextView2;
+    ImageView imgHome;
+    private AnimationDrawable frameAnimation;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        animTextView = view.findViewById(R.id.diverTextView);
+        animTextView2 = view.findViewById(R.id.text_home);
+
+        startAnimation();
+
+        // IMAGENES DE LA HOME ALTERNAS
+        imgHome = (ImageView) view.findViewById(R.id.fotoPortada);
+        imgHome.setImageResource(0);
+        // set the xml with images
+        imgHome.setBackgroundResource(R.drawable.animation);
+        // get the background to show the animation
+        frameAnimation = (AnimationDrawable) imgHome.getBackground();
+        frameAnimation.start();
+    }
+
+    private void startAnimation(){
+
+        Animation animation = AnimationUtils.loadAnimation(requireActivity(), R.anim.anim);
+        animTextView.startAnimation(animation);
+        animTextView2.startAnimation(animation);
+    }
+}
